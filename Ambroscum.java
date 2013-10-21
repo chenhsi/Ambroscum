3c42d60d8d8262bd6c0dac5fc58442722f412544
Jing-Lun Edward Gao <edward.jl.gao@berkeley.edu>
2013-10-21 14:47:23 -0700

    Update Ambroscum.java

--- Ambroscum.java
+++ Ambroscum.java
@@ -1,5 +1,11 @@
 package ambroscum;
 
+/**
+ * The entry point to the program.
+ * 
+ * @author Chenhsi Steven Bi, Jing-Lun Edward Gao
+ * @version 1.0
+ */
 public class Ambroscum
 {
 	public static void main(String[] args)
