90c89b16415b005675ce9180b168b5bad861c721
Jing-Lun Edward Gao <edward.jl.gao@berkeley.edu>
2013-11-22 23:35:15 -0800

    Modified Line to use the token stream

--- lines/Line.java
+++ lines/Line.java
@@ -27,7 +27,20 @@ public abstract class Line
 		{
 			stream.removeFirst();
 			return new PrintLine(stream, token.length() == 7);
-		}
+		} else {
+			// Look-ahead to see if we hit '=' before the next line
+			Iterator<Token> streamIter;
+			while (streamIter.hasNext()) {
+				Token next = streamIter.next();
+				String str = next.getString();
+				if (str.equals("=")) {
+					return new AssignmentLine(stream);
+				} else if (str.equals("\n")) {
+					throw new UnsupportedOperationException("Call expressions not implemented!");
+				}
+			}
+			//int index = first.indexOf(" = ");
+			//return new AssignmentLine(first.substring(0, index), first.substring(index + 3));
 //		if (code.contains(" = "))
 //		{
 //			int index = code.indexOf(" = ");
