44cc56d1174fad29050bf03f378a4980c38d4ebe
Jing-Lun Edward Gao <edward.jl.gao@berkeley.edu>
2013-11-27 15:03:03 -0800

    Added very basic negative test case handling
    See tests\06 basic-error.ambr for example. In the .correct file, use the
    "ERROR: " prefix to show the error location. Optionally, add the type of
    error to expect (e.g. "ambroscum.errors.SyntaxError").

--- tests/AutoTester.java
+++ tests/AutoTester.java
@@ -1,5 +1,9 @@
 /**
  * Runs test cases from the /tests folder.
+ *
+ * USAGE: java AutoTester
+ * Place test files in the /tests directory.
+ * Test file containing code should be called *.ambr; the correct output should be in *.ambr.correct.
  * 
  * @author Jing-Lun Edward Gao
  * @version 1.0
@@ -15,7 +19,8 @@ public class AutoTester {
 	
 	public static final String TEST_DIRECTORY = System.getProperty("user.dir") + "/tests",
 								TEST_EXTENSION = ".ambr",
-								CORRECT_EXTENSION = ".correct";
+								CORRECT_EXTENSION = ".correct",
+								ERROR_PREFIX = "ERROR: ";
 	
 	public static void main(String[] args) throws IOException {
 		PrintStream systemOut = System.out;
@@ -33,12 +38,13 @@ public class AutoTester {
 			PipedInputStream pipeIn = new PipedInputStream(pipeOut);
 			System.setOut(new PrintStream(pipeOut));
 
-			Interpreter.interpret(file, false);
-			
-			Scanner correctFile = new Scanner(new File(file.getPath() + CORRECT_EXTENSION));
-			StringBuilder correctOutput = new StringBuilder();
-			while (correctFile.hasNextLine()) {
-				correctOutput.append(correctFile.nextLine() + "\n");
+			// A hack for now.
+			boolean hadError = false;
+			try {
+				Interpreter.interpret(file, false);
+			} catch (Exception ex) {
+				ex.printStackTrace(System.out);
+				hadError = true;
 			}
 			
 			StringBuilder testOutput = new StringBuilder();
@@ -46,9 +52,41 @@ public class AutoTester {
 				int read = pipeIn.read();
 				testOutput.append((char) read);
 			}
-			String correctOutputStr = correctOutput.toString().replaceAll(System.getProperty("line.separator"), "\n").trim();
 			String testOutputStr = testOutput.toString().replaceAll(System.getProperty("line.separator"), "\n").trim();
-			if (correctOutputStr.equals(testOutputStr)) {
+			
+			StringBuilder correctOutputStrBldr = new StringBuilder();
+			
+			Scanner correctFileScanner = new Scanner(new File(file.getPath() + CORRECT_EXTENSION));
+			Scanner testOutputScanner = new Scanner(testOutputStr);
+			
+			boolean correct = true;
+			while (correctFileScanner.hasNextLine()) {
+				String correctLine = correctFileScanner.nextLine();
+				correctOutputStrBldr.append(correctLine + "\n");
+				String testLine = testOutputScanner.hasNextLine() ? testOutputScanner.nextLine() : null;
+				// Not enough output - incorrect
+				if (testLine == null) {
+					correct = false;
+					break;
+				}
+				// Check if this line was supposed to be an error.
+				if (correctLine.indexOf(ERROR_PREFIX) == 0) {
+					String expectedError = correctLine.substring(ERROR_PREFIX.length());
+					// Either not an error, or the wrong error
+					if (testLine.indexOf(expectedError) != 0) {
+						correct = false;
+					}
+					break;
+				}
+				// Incorrect output
+				if (!correctLine.equals(testLine)) {
+					correct = false;
+					break;
+				}
+			}
+			
+			String correctOutputStr = correctOutputStrBldr.toString().replaceAll(System.getProperty("line.separator"), "\n").trim();
+			if (correct) {
 				System.err.println("Tests passed for " + fileName);
 			} else {
 				System.err.println("Incorrect output for " + fileName);
@@ -56,8 +94,8 @@ public class AutoTester {
 				System.err.println(testOutputStr);
 				System.err.println("-----------");
 				System.err.println(correctOutputStr);
-				System.err.println("======================");
 			}
+			System.err.println("======================");
 		}
 	}
 }
